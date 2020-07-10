23e8b34f2c126dfc1955bb252bf6d3ae90381a7c
Julian Vargas <julian.vargas@Elinvar.de>
2020-07-10 23:16:16 +0200

    adds resource for dashborad

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :dashboard, only: [:index]
   root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_195029) do
+ActiveRecord::Schema.define(version: 2020_07_10_210847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
