e8e4ffba9392e93df707090d9344e0c3cd678ab3
Julian Vargas <julian.vargas@Elinvar.de>
2020-07-15 10:36:36 +0200

    paginated columns working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @candidates = Candidate
+    @pending =     Candidate.where(status: 'pending'    ).order(id: :asc).page(params['pending']    ).per(3)
+    @interviewed = Candidate.where(status: 'interviewed').order(id: :asc).page(params['interviewed']).per(3)
+    @hired =       Candidate.where(status: 'hired'      ).order(id: :asc).page(params['hired']      ).per(3)
   end
 end
