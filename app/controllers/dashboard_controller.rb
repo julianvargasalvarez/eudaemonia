046fe5aea2395635937bfd2b6e61aa134dd49353
Julian Vargas <julian.vargas@Elinvar.de>
2020-07-17 21:54:35 +0200

    cleanup code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,9 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @pending =     Candidate.where(status: 'pending'    ).order(id: :asc).page(params['pending']    ).per(10)
-    @interviewed = Candidate.where(status: 'interviewed').order(id: :asc).page(params['interviewed']).per(3)
-    @hired =       Candidate.where(status: 'hired'      ).order(id: :asc).page(params['hired']      ).per(3)
+    @pending =     Candidate.where(status: 'pending'    ).order(id: :asc).page(params['pending']    ).per(5)
+    @interviewed = Candidate.where(status: 'interviewed').order(id: :asc).page(params['interviewed']).per(5)
+    @hired =       Candidate.where(status: 'hired'      ).order(id: :asc).page(params['hired']      ).per(5)
 
     respond_to do |format|
       format.html
