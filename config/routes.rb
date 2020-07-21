75628ee76edd6cf675b10092818a5e14ef7913f0
Julian Vargas <julian.vargas@Elinvar.de>
2020-07-21 08:53:45 +0200

    adds new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :dashboard, only: [:index]
+
+
+  get '/dashboard_con_stimulus', to: 'dashboard#index'
+  get '/dashboard_con_stimulus_reflex', to: 'dashboard#index'
+  get '/dashboard_con_react_serverside_rendering', to: 'dashboard#index'
+  get '/dashboard_con_react_clientside_rendering', to: 'dashboard#index'
+
   resources :reports, only: [:index]
   resources :candidate
   root to: "home#index"
