fa476eb510ed2c2ad5f7092a9f144182d87e65f6
Julian Vargas <julian.vargas@Elinvar.de>
2020-07-23 21:59:27 +0200

    removes link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-
   get '/dashboard_con_stimulus', to: 'dashboard#index'
-  get '/dashboard_con_react', to: 'dashboard#index'
 
   resources :reports, only: [:index]
   resources :candidate
